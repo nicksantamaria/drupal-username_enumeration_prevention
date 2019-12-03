c98d24b5c0cd18400b89ce99e1f6e61c1ccd38ad
danph <danph@81431.no-reply.drupal.org>
2019-12-04 09:34:15 +1100

    Issue #3097461 by dpi: Show 404 if 403 on user link template routes

--- tests/src/Functional/UserRouteTest.php
+++ tests/src/Functional/UserRouteTest.php
@@ -13,28 +13,64 @@ use Drupal\Tests\BrowserTestBase;
 class UserRouteTest extends BrowserTestBase {
 
   /**
-   * Modules to enable.
-   *
-   * @var array
+   * {@inheritdoc}
    */
-  public static $modules = ['username_enumeration_prevention'];
+  public static $modules = ['user'];
 
   /**
-   * Asserts user routes returns 404 response code for anon users.
+   * Tests user routes.
+   *
+   * @param string $routeId
+   *   Route ID.
+   * @param array $routeParameters
+   *   Additional route parameters.
+   * @param array $modules
+   *   Enable these modules.
+   * @param int $expectedStatus
+   *   Expected HTTP status.
+   *
+   * @dataProvider providerTestUserRoutes
    */
-  public function testCanonicalRoute() {
-    $this->drupalGet(Url::fromRoute('entity.user.canonical', ['user' => 1]));
-    $this->assertSession()->statusCodeEquals(404);
+  public function testUserRoutes(string $routeId, array $routeParameters, array $modules, int $expectedStatus) {
+    $user = $this->drupalCreateUser();
+    \Drupal::service('module_installer')->install($modules);
+    $routeParameters['user'] = $user->id();
+    $this->drupalGet(Url::fromRoute($routeId, $routeParameters));
+    $this->assertSession()->statusCodeEquals($expectedStatus);
+  }
 
-    $this->drupalGet(Url::fromRoute('user.cancel_confirm', ['user' => 1]));
-    $this->assertSession()->statusCodeEquals(404);
+  /**
+   * Provides routes for testing.
+   */
+  public function providerTestUserRoutes(): array {
+    // user.module.
+    $scenarios['user canonical'] = ['entity.user.canonical'];
+    $scenarios['user edit form'] = ['entity.user.edit_form'];
+    $scenarios['user cancel form'] = ['entity.user.cancel_form'];
+    $scenarios['cancel confirm 1'] = ['user.cancel_confirm'];
+    $scenarios['cancel confirm 2'] = [
+      'user.cancel_confirm',
+      ['timestamp' => 0, 'hashed_pass' => 'foo'],
+    ];
 
-    $this->drupalGet(Url::fromRoute('user.cancel_confirm', [
-      'user'  => 1,
-      'timestamp' => 0,
-      'hashed_pass' => 'foo',
-    ]));
-    $this->assertSession()->statusCodeEquals(404);
+    // contact.module.
+    $scenarios['contact user contact form'] = [
+      'entity.user.contact_form',
+      [],
+      ['contact'],
+    ];
+
+    $data = [];
+    foreach ($scenarios as $key => $scenario) {
+      $scenario = $scenario + array_fill_keys(range(1, 2), []);
+      // Test Drupal user routes return 403 out of the box. We use these to test
+      // if core changed the behaviour of these routes, and now 404.
+      $data['without uep ' . $key] = $scenario + [3 => 403];
+      $scenario[2][] = 'username_enumeration_prevention';
+      $data['with uep ' . $key] = $scenario + [3 => 404];
+    }
+
+    return $data;
   }
 
 }
