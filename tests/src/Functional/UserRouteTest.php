a6128637843fef867dc227711d6602d17607fe08
Nick Santamaria <nick.santamaria@previousnext.com.au>
2019-12-03 19:59:46 +1100

    Updated tests with user.cancel_confirm route.

--- tests/src/Functional/UserRouteTest.php
+++ tests/src/Functional/UserRouteTest.php
@@ -13,14 +13,24 @@ use Drupal\Tests\BrowserTestBase;
 class UserRouteTest extends BrowserTestBase {
 
   /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = ['username_enumeration_prevention'];
+
+  /**
    * Asserts user routes returns 404 response code for anon users.
    */
   public function testCanonicalRoute() {
-    $this->drupalGet(Url::fromRoute('entity.user.canonical'));
+    $this->drupalGet(Url::fromRoute('entity.user.canonical', ['user' => 1]));
+    $this->assertSession()->statusCodeEquals(404);
+
+    $this->drupalGet(Url::fromRoute('user.cancel_confirm', ['user' => 1]));
     $this->assertSession()->statusCodeEquals(404);
 
     $this->drupalGet(Url::fromRoute('user.cancel_confirm', [
-      'user'  => '1',
+      'user'  => 1,
       'timestamp' => 0,
       'hashed_pass' => 'foo',
     ]));
