a00048d94d785dc6a452b34dac8d6e15f1399124
Nick Santamaria <nicholas.santamaria@dpc.vic.gov.au>
2022-12-13 10:22:24 +1100

    Replaced circle build with one from Drevops.

--- .circleci/lint.sh
+++ .circleci/lint.sh
@@ -5,7 +5,35 @@
 
 set -e
 
-MODULE=$(basename -s .info.yml -- ./*.info.yml)
+#-------------------------------------------------------------------------------
+# Variables (passed from environment; provided for reference only).
+#-------------------------------------------------------------------------------
 
-echo "==> Lint code"
-build/vendor/bin/phpcs -s --standard=Drupal,DrupalPractice --extensions=module,php,install,inc,test,info.yml,js "build/web/modules/${MODULE}"
+# Directory where Drupal site will be built.
+BUILD_DIR="${BUILD_DIR:-build}"
+
+# Module name, taken from .info file.
+MODULE="$(basename -s .info.yml -- ./*.info.yml)"
+
+#-------------------------------------------------------------------------------
+
+echo "==> Lint code for module $MODULE."
+echo "  > Running PHPCS."
+build/vendor/bin/phpcs \
+  -s \
+  -p \
+  --standard=Drupal,DrupalPractice \
+  --extensions=module,php,install,inc,test,info.yml,js \
+  "build/web/modules/${MODULE}"
+
+echo "  > Running drupal-check."
+build/vendor/bin/drupal-check \
+  --drupal-root=build/web \
+  "build/web/modules/${MODULE}"
+
+echo "  > Running Drupal Rector."
+pushd "build" >/dev/null || exit 1
+vendor/bin/rector process \
+  "web/modules/${MODULE}" \
+  --dry-run
+popd >/dev/null || exit 1
